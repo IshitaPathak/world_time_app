98cf7b195b76de931358195b99f97a62c2da7e39
IshitaPathak <pathakishita111@gmail.com>
2023-02-24 01:01:15 +0530

    poping data from choose location page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,9 @@ class _HomeState extends State<Home> {
   Map? data = {};
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context)?.settings.arguments as Map?;
+    data = data!.isNotEmpty
+        ? data
+        : ModalRoute.of(context)?.settings.arguments as Map?;
 
     print(data);
 
@@ -33,8 +35,17 @@ class _HomeState extends State<Home> {
           child: Column(
             children: [
               TextButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
+                onPressed: () async {
+                  dynamic result =
+                      await Navigator.pushNamed(context, '/location');
+                  setState(() {
+                    data = {
+                      'time': result['time'],
+                      'location': result['location'],
+                      'isDayTime': result['iDayTime'],
+                      'flag': result['flag']
+                    };
+                  });
                 },
                 icon: Icon(
                   Icons.edit_location,
